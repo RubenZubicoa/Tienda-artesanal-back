d819d442cf2d845f51f4cbe6bd0224a93d4e5054
Ruben <rzubicoa@eservicios.indra.es>
2025-12-21 09:24:58 +0100

    feat: eliminar carpeta dist

--- dist/db/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectionString = void 0;
-exports.connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/tienda';
