b39dcb9522c922f41cb7dc6111c6a84ab087b5fe
Ruben <rubenzubicoatic@gmail.com>
2026-04-04 14:30:46 +0200

    feat: cambiar email de artis

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -6,12 +6,12 @@ export async function sendEmail(to: string, subject: string, html: string) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "rubenzubicoatic@gmail.com",
-      pass: "vbrr pwhs wkjc nbjk"
+      user: process.env.NODEMAILER_MAIL,
+      pass: process.env.NODEMAILER_PASS,
     }
   });
     const info = await transporter.sendMail({
-        from: '"Tienda artesanal" <rubenzubicoatic@gmail.com>',
+        from: '"ARTIS" <' + process.env.NODEMAILER_MAIL + '>',
         to: to,
         subject: subject,
         html: html,
